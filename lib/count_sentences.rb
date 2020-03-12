71b9973ad316e767b4103360e00940cdf48a041e
replacem3nts <donny.landis@gmail.com>
2020-03-11 23:07:18 -0400

    updates file so all tests pass

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
-  def count_sentences
+  
 
+  def count_sentences
+    a = self.split.select {|chr| chr.include?("?") || chr.include?("!") || chr.include?(".")}
+    a.count
   end
 end
