f2bb220a7964b12e82f18b3ae31a64cbc8d70685
github username <gabriellealexa>
2020-03-13 17:43:26 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
+    return true if self.end_with?(".")
+    return false if !self.end_with?(".")
     
   end
 
   def question?
+    return true if self.end_with?("?")
+    return false if !self.end_with?("?")
 
   end
 
   def exclamation?
+    return true if self.end_with?("!")
+    return false if !self.end_with?("!")
 
   end
 
   def count_sentences
-
-  end
+    self.split(/\.|\?|\!/).delete_if {|w| w.size <2}.size 
+  end 
 end
